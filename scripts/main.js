f077c51e89f746b53f90bbacf298e11c1b13f343
Chris Heise <111378122+cheisenmhu@users.noreply.github.com>
2024-02-23 12:56:51 -0700

    Initialize the project
    Create minimum required files with some pseudo-code.

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+/*
+ * Programmer: Chris Heise (https://github.com/crHeise)
+ * Date: 23 February, 2024
+ * Project: The Odin Project - Weather Forecast Application
+ * File: main.js
+ * Purpose: This is the main JavaScript file for the Weather Forecast Application.
+ */
+
+// Import the necessary modules
+
+// Setup the DOM elements/variables
+
+// Define a function to get the user's location from a button press
+
+// Call the necessary functions to get the weather forecast & gif for the user's location
+
+// Display the information on the screen for the user
