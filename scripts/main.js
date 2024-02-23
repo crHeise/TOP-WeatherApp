586b66db04ebb66c33fdc8e8d02fa203a55857bf
Chris Heise <111378122+cheisenmhu@users.noreply.github.com>
2024-02-23 14:39:03 -0700

    Add basic functionality

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,48 @@
  * Date: 23 February, 2024
  * Project: The Odin Project - Weather Forecast Application
  * File: main.js
- * Purpose: This is the main JavaScript file for the Weather Forecast Application.
+ * Purpose: Manipulate the DOM in response to user actions.
  */
 
-// Import the necessary modules
+// Set global variables for API keys (not a good practice, but it's fine for this project)
+const WEATHER_API_KEY = "7f68d477db6949f78c2190831242302";
+const GIPHY_API_KEY = "Pfi361TMcaa8GoBSK3IfXMW0MayJMHJV";
+
+// Set global variables for the API URLs
+let weatherURL = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=`;
+let giphyURL = `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=`;
 
 // Setup the DOM elements/variables
+let cityName = document.getElementById('city-name');
+let submitButton = document.getElementById('get-weather-button');
+let city = document.getElementById('city');
+let temperature = document.getElementById('temperature');
+let feelsLike = document.getElementById('feels-like');
+let conditions = document.getElementById('conditions');
+let conditionsGif = document.getElementById('conditions-gif');
 
-// Define a function to get the user's location from a button press
+async function getWeatherData(cityName) {
+    const response = await fetch(`${weatherURL}${cityName}`);
+    const weatherData = await response.json();
 
-// Call the necessary functions to get the weather forecast & gif for the user's location
+    city.textContent = weatherData.location.name; 
+    temperature.textContent = `Temp: ${weatherData.current.temp_f}°F`;
+    feelsLike.textContent = `Feels like: ${weatherData.current.feelslike_f}°F`;
+    conditions.textContent = `Conditions: ${weatherData.current.condition.text}`;
 
-// Display the information on the screen for the user
+    let currentConditions = String(weatherData.current.condition.text);
+    let conditionsTrimmed = currentConditions.replace(/\s+/g, '');
+
+    fetch(`${giphyURL}${conditionsTrimmed}weather`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response.data.images.original.url);
+        conditionsGif.setAttribute('src', response.data.images.original.url);   // not currently working: third party cookies issue
+    });
+}
+
+submitButton.addEventListener('click', () => {
+    getWeatherData(cityName.value);
+});
